7de95f4e436f974bebf4b442d7cfe7b5417298ea
HiPatil <hipatil1998@gmail.com>
2020-07-26 15:16:47 +0530

    added rotate function

--- yolov4_test.py
+++ yolov4_test.py
@@ -9,6 +9,16 @@ from tool.plateprocessing import find_coordinates, plate_detect
 from tool.utils import alphanumeric_segemntor,plot_boxes_cv2
 from tool.torch_utils import *
 
+def rotate(olist, rot):
+	olistnew = []
+	for element in olist:
+		x,y,c = element[0], element[1], element[2]
+		templist = np.array([[element[0]], [element[1]]])
+		templist = np.matmul(rot, templist)
+		x = templist[0][0]
+		y = templist[1][0]
+		olistnew = olistnew + [(x,y,c)]
+	return olistnew
 
 def findstring(elements, threshold):
 	elements.sort(key = lambda x: x[1])
@@ -74,7 +84,7 @@ use_cuda = True
 #################### PLATE ####################
 
 cfg_v4 = 'pytorch-YOLOv4-master/cfg/yolo-obj.cfg'
-weight_v4 = 'num_plate.weights'
+weight_v4 = '/home/himanshu/Downloads/yolo-obj_last.weights'
 
 m = DarknetYolov4(cfg_v4)
 m.load_weights(weight_v4)
@@ -85,7 +95,7 @@ print('Loading weights from %s... Done!' % (weight_v4))
 #################### DIGIT ####################
 
 cfg_v4_alpha = 'pytorch-YOLOv4-master/cfg/digit.cfg'
-weight_v4_alpha = 'yolo-obj_afterbeforefinal.weights'
+weight_v4_alpha = '/home/himanshu/Downloads/alphanumeric.weights'
 
 m_alpha = DarknetYolov4(cfg_v4_alpha)
 m_alpha.load_weights(weight_v4_alpha)
@@ -96,11 +106,14 @@ print('Loading weights from %s... Done!' % (weight_v4_alpha))
 
 if use_cuda:
 	m.cuda()
-	# m_alpha.cuda()
+	m_alpha.cuda()
 
-
+size = (1280, 720)
 # cap = cv2.VideoCapture('C:/Users/rohit/Videos/1.mp4')
 cap = cv2.VideoCapture('1.mp4')
+plate_1_writer = cv2.VideoWriter('plate_1.avi',  cv2.VideoWriter_fourcc(*'MJPG'), 25, size) 
+
+# plate_2_writer = cv2.VideoWriter('plate_2.avi', cv2.VideoWriter_fourcc(*'MJPG'), 25, size) 
 
 cap.set(3, 1280)
 cap.set(4, 720)
@@ -117,6 +130,7 @@ while True:
 	result_img = plot_boxes_cv2(img, boxes[0],fontScale=0.5,thick=2, 
 				savename=False, class_names=class_names_alpha)
 	cv2.imshow('Yolo plate detection', result_img)
+	plate_1_writer.write(result_img)
 
 	if len(boxes[0]) > 0 :
 		x1, y1, x2, y2 = find_coordinates(img, boxes[0])
@@ -126,14 +140,15 @@ while True:
 
 		sized = cv2.resize(plate_bb, (m_alpha.width, m_alpha.height))
 		sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
-		confidence = 0.4
-		boxes = do_detect(m_alpha, sized, confidence , 0.6, False)
+		confidence = 0.85
+		boxes = do_detect(m_alpha, sized, confidence , 0.6, use_cuda)
 		finish = time.time()
 		print('Predicted in %f seconds.' % (finish - start))
 		digit_on_plate = plot_boxes_cv2(plate_bb, boxes[0],fontScale=0.5,thick=2, 
 						savename=False, class_names=class_names_alpha, color=(0,0,0))
 
 		cv2.imshow('digit_on_plate', digit_on_plate)
+
 		alphanumerics,x_c_list,y_c_list = alphanumeric_segemntor(plate_bb, boxes[0],class_names=class_names_alpha)
 
 		## Sort plate on basis of x axis
